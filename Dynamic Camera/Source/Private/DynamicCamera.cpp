6c5553bbd6d12d7c05ab8d073ad9202fb361ba84
onederfoo <onederfoo@icloud.com>
2021-02-10 07:25:05 -0800

    comments
    Added comments to the private file

--- Dynamic Camera/Source/Private/DynamicCamera.cpp
+++ Dynamic Camera/Source/Private/DynamicCamera.cpp
@@ -37,6 +37,7 @@ void ADynamicCamera::BeginPlay()
 														::OnOverlapEnd);
 }
 
+/** Used to Reset the camera back to the player after the initial SetViewTragetWithBlend */
 void ADynamicCamera::ResetCamera()
 {
 	UE_LOG(LogTemp, Warning, TEXT("ResetCamera() called"));
@@ -44,6 +45,7 @@ void ADynamicCamera::ResetCamera()
 	Controller->SetViewTargetWithBlend(EpicCharacter, CameraResetTimer);
 }
 
+/** Enables movement once the blend is over and the camera is positioned where the player is */
 void ADynamicCamera::EnableMovement()
 {
 	UE_LOG(LogTemp, Warning, TEXT("EnableMovement() called"));
@@ -68,7 +70,7 @@ void ADynamicCamera::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent,
 									int32 OtherBodyIndex, bool bFromSweep,
 									const FHitResult& SweepResult)
 {
-	/*
+	/**
 	 *	Reference to the character (can be any native or blueprint character)  
 	 *	cast it to the otherActor to call the onOverlapBegin function on.
 	 */
@@ -78,7 +80,10 @@ void ADynamicCamera::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent,
 		UE_LOG(LogTemp, Warning, TEXT("Object Triggered through OnOverlapBegin()"));
 		APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
 		{
-			/*	Do not use DisableInput. Using DisableINput will retain the player's 
+			/**	
+			 *	Disabling character movement and changing the camera view to wherever 
+			 *	this Actors "Camera" component is with a blend. 
+			 *	Do not use DisableInput. Using DisableINput will retain the player's 
 			 *	movement speed before the cut scene, so if you were running, after the cut scene
 			 *	your character will continue running and that's not common game behavior.
 			 */
