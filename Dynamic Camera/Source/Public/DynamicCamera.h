dfc36da8e7722e1e2178c45961d05cdcf014e776
Alan Meyghan <meyghan.alan@gmail.com>
2022-02-04 10:43:34 -0800

    Cleaned up the header file. All functionality of the blending in and out is now done in one function that takes advantage of lambads within the function to achieve the same blending as before. Cleaner and more performant.

--- Dynamic Camera/Source/Public/DynamicCamera.h
+++ Dynamic Camera/Source/Public/DynamicCamera.h
@@ -2,62 +2,46 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "Camera/CameraComponent.h"
-#include "GameFramework/Character.h"
-
 #include "DynamicCamera.generated.h"
 
+class UCharacterMovementComponent;
+class APlayerController;
+
 UCLASS()
-class /*YOUR_PROJECT_NAME_API*/ ADynamicCamera : public AActor
+class /*YOURGAME_API*/ ABOTDynamicCamera : public AActor
 {
 	GENERATED_BODY()
-
-public:
-	/*Constructor*/
-	ADynamicCamera();
 	
+public:	
+	/** Constructor */
+	ADynamicCamera();
+
+protected:
 	virtual void BeginPlay() override;
 
 private:
+	UFUNCTION(BlueprintCallable)
+	void BlendCameraInAndOut(ACharacter* InCharacter, UCharacterMovementComponent* InCharacterMovement);
 
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
+	/** Components */
 	UPROPERTY(EditDefaultsOnly, Category = "Components")
 	class USceneComponent* CameraRoot;
-	
 	// The component used to trigger the camera 
 	UPROPERTY(EditDefaultsOnly, Category = "Components")
 	class UBoxComponent* OverlapComponent;
 	UPROPERTY(EditDefaultsOnly, Category = "Components")
 	class UCameraComponent* Camera;
-
-	// The reference character that interacts with the camera to activate the OverlapComponent
-	UPROPERTY()
-	class ACharacter* YourCharacter;
-
+	
 	// The amount of blend time for the camera movement
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Timers", meta = (AllowPrivateAccess = "true"))
 	float BlendTime;
 	
 	// The amount of time it takes for the delay to reset the camera back to its original position
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Timers", meta = (AllowPrivateAccess = "true"))
-	float CameraResetTimer;
-
-	// Resetting the character movement after the cut-scene-like camera movement is done
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Timers", meta = (AllowPrivateAccess = "true"))
-	float MovementResetTimer;
-
-	UFUNCTION(BlueprintCallable)
-	void ResetCamera();	
-	UFUNCTION(BlueprintCallable)
-	void EnableMovement();
-
-	UFUNCTION()
-	void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent,
-						AActor* OtherActor, UPrimitiveComponent* OtherComp,
-						int32 OtherBodyIndex, bool bFromSweep,
-						const FHitResult& SweepResult);
-	UFUNCTION()
-	void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, 
-					  AActor* OtherActor, UPrimitiveComponent* OtherComp, 
-					  int32 OtherBodyIndex);
-
+	float CameraBlendTimer;
 };
